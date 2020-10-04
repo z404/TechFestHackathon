c0fb46d324f9bff7609f70d2e16d8167afd0a79d
Anish Raghavendra <anishr890@gmail.com>
2020-10-04 18:35:51 +0530

    2nd commit

--- shortener.py
+++ shortener.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import numpy as np
 from IPython.display import display
@@ -22,28 +21,3 @@ for i in range(len(emails)):
             leftoverdf = leftoverdf.append(emails.iloc[i])
 
 print(countdf)
-=======
-import pandas as pd
-import numpy as np
-from IPython.display import display
-import email
-import json
-
-filepath = "cleaned.csv"
-emails = pd.read_csv(filepath)
-
-countdf = {'humor':0, 'resumes':0, 'hr':0, 'personal':0, 'universities':0}
-
-newdf = pd.DataFrame(columns = emails.columns)
-leftoverdf = pd.DataFrame(columns = emails.columns)
-
-for i in range(len(emails)):
-    if emails.iloc[i]['X-Folder'] in list(countdf.keys()):
-        countdf[emails.iloc[i]['X-Folder']] += 1
-        if countdf[emails.iloc[i]['X-Folder']] <= 75:
-            newdf = newdf.append(emails.iloc[i])
-        else:
-            leftoverdf = leftoverdf.append(emails.iloc[i])
-
-print(countdf)
->>>>>>> 65c1c1cebbe728ea79a841cb0075e400165f3530
